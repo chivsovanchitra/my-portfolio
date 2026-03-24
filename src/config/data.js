ef4b956db41b8edac7374b8b77696e09e2911970
chivsovanchitra <chivsovanchetra@gmail.com>
2026-03-24 17:13:32 +0700

    fix image path

--- src/config/data.js
+++ src/config/data.js
@@ -6,20 +6,20 @@ export const userInfo = {
 };
 
 export const projects = [
-  { title: "RemoteYourWork", img: "src/assets/project-1.png" },
-  { title: "Memoire Vault", img: "src/assets/project-2.png" },
-  { title: "Receipies", img: "src/assets/project-3.png" },
+  { title: "RemoteYourWork", img: "./src/assets/project-1.png" },
+  { title: "Memoire Vault", img: "./src/assets/project-2.png" },
+  { title: "Receipies", img: "./src/assets/project-3.png" },
 ];
 
 export const aboutMe = {
   experience: {
     title: "Experience",
-    image: "src/assets/experience.png",
+    image: "./src/assets/experience.png",
     desc: "2+ years Frontend",
   },
   education: {
     title: "Education",
-    image: "src/assets/education.png",
+    image: "./src/assets/education.png",
     desc: "Bachelor Degree",
   },
 };
